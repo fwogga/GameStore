86250356563c19c2087fa75071eaddaf0f093d4c
fwogga <Miguelalmeida91120@gmail.com>
2025-12-04 04:28:20 +0000

    forgor

--- app/src/main/java/pt/iade/ei/gamestore/controller/MainActivity.kt
+++ app/src/main/java/pt/iade/ei/gamestore/controller/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import pt.iade.ei.gamestore.R
 import pt.iade.ei.gamestore.model.ServidorFake
